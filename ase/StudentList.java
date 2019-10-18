dfaa79bfa833936166708e58cbee14ffd2677d17
Mohammad Kasedullah <kasid.raj@gmail.com>
2019-10-18 16:56:17 +0600

    STEP #3.Makes improvements to variable names

--- ase/StudentList.java
+++ ase/StudentList.java
@@ -13,12 +13,12 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
+			String contents = fileStream.readLine();
+			String words[] = contents.split(",");
+			for(String word : words) { System.out.println(word); }
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");
 		}
@@ -26,15 +26,15 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			System.out.println(r);
-			String i[] = r.split(",");	
+			String contents = fileStream.readLine();
+			System.out.println(contents);
+			String words[] = contents.split(",");
 			Random x = new Random();
 				int y = x.nextInt();
-					System.out.println(i[y]);
+					System.out.println(words[y]);
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");			
 		}
@@ -59,15 +59,15 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");	
+			String contents = fileStream.readLine();
+			String words[] = contents.split(",");
 			boolean done = false;
 			String t = args[0].substring(1);
-			for(int idx = 0; idx<i.length && !done; idx++) {
-				if(i[idx].equals(t)) {
+			for(int idx = 0; idx<words.length && !done; idx++) {
+				if(words[idx].equals(t)) {
 					System.out.println("We found it!");
 						done=true;
 				}
@@ -79,11 +79,11 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String D = s.readLine();
-			char a[] = D.toCharArray();			
+			String contents = fileStream.readLine();
+			char a[] = contents.toCharArray();
 			boolean in_word = false;
 			int count=0;
 			for(char c:a) {
